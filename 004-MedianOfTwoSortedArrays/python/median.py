cca5a17b615b567f8d5fd917033e40e7b775c7d4
samguns <wang.vai@gmail.com>
2018-10-15 15:50:29 +0800

    Start 004

---
+++ 004-MedianOfTwoSortedArrays/python/median.py
@@ -0,0 +1,37 @@
+import json
+
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            nums1 = stringToIntegerList(line);
+            line = next(lines)
+            nums2 = stringToIntegerList(line);
+
+            ret = Solution().findMedianSortedArrays(nums1, nums2)
+
+            out = str(ret);
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
