8daf8842fa336e8975eecfa35e2aa300f025b8ae
samguns <wang.vai@gmail.com>
2018-10-16 15:57:54 +0800

    Day2 of Two Sum, Day1 of 004

--- 004-MedianOfTwoSortedArrays/python/median.py
+++ 004-MedianOfTwoSortedArrays/python/median.py
@@ -7,6 +7,43 @@ class Solution:
         :type nums2: List[int]
         :rtype: float
         """
+        m = len(nums1)
+        n = len(nums2)
+        A = nums1
+        B = nums2
+        if m > n:
+            A, B, m, n = nums2, nums1, n, m
+
+        imin, imax = 0, m
+        middle = int((m+n+1)/2)
+
+        while (imin <= imax):
+            i = int((imin + imax) / 2)
+            j = middle - i
+
+            if i < m and B[j-1] > A[i]:
+                imin = i + 1
+            elif i > 0 and A[i-1] > B[j]:
+                imax = i - 1
+            else:
+                if i == 0:
+                    max_of_left = B[j-1]
+                elif j == 0:
+                    max_of_left = A[i-1]
+                else:
+                    max_of_left = max(A[i-1], B[j-1])
+
+                if (m + n) % 2 == 1:
+                    return max_of_left
+
+                if i == m:
+                    min_of_right = B[j]
+                elif j == n:
+                    min_of_right = A[i]
+                else:
+                    min_of_right = min(A[i], B[j])
+
+                return (max_of_left + min_of_right) / 2.0
 
 def stringToIntegerList(input):
     return json.loads(input)
@@ -22,13 +59,13 @@ def main():
     while True:
         try:
             line = next(lines)
-            nums1 = stringToIntegerList(line);
+            nums1 = stringToIntegerList(line)
             line = next(lines)
-            nums2 = stringToIntegerList(line);
+            nums2 = stringToIntegerList(line)
 
             ret = Solution().findMedianSortedArrays(nums1, nums2)
 
-            out = str(ret);
+            out = str(ret)
             print(out)
         except StopIteration:
             break
